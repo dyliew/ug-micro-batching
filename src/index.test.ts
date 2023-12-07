7d033f7e6c8789acf603a525ed495c16d38d6448
Yang Liew <yang.liew@rea-group.com>
2023-12-07 23:27:40 +1100

    Initial repo setup

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { text } from './';
+
+describe('index', () => {
+  it('should pass', () => {
+    expect(text).toBe('test');
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export interface Test {
+  name: string;
+  age: number;
+}
+
+export const text = 'test';
